f60cfbf2b88ed533ba31d8c2b4e2bbe1e884635d
leo <chichali374@gmail.com>
2024-03-19 11:04:57 +0800

    Updates

--- assets/NotFound-X3x--dsX.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as n,c as t,F as s,a as o}from"./index-zLiU2U1g.js";const c={},_=o("h1",null,"Oops!",-1),a=o("h2",null,"404",-1),l=o("h3",null,"page not found!",-1),r=o("p",null,"網頁網址有錯喔~請確認是否正確",-1);function d(u,h){return n(),t(s,null,[_,a,l,r],64)}const i=e(c,[["render",d]]);export{i as default};
