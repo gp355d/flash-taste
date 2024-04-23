9e3d55f984613825efa633ba5d6199203fe1e43d
leo <chichali374@gmail.com>
2024-04-23 23:52:39 +0800

    Updates

--- assets/DeleteProductModal-aFvwOzJm.js
+++ assets/DeleteProductModal-aFvwOzJm.js
@@ -1,4 +1,4 @@
-import{K as I,M as Dt,_ as Tt,o as H,c as G,a as L,t as Y,d as J,F as lt}from"./index-Tl3ByFI-.js";var At={exports:{}},X={exports:{}},Z={exports:{}};/*!
+import{K as I,M as Dt,_ as Tt,o as H,c as G,a as L,t as Y,d as J,F as lt}from"./index-8J3nc3ZK.js";var At={exports:{}},X={exports:{}},Z={exports:{}};/*!
   * Bootstrap data.js v5.3.3 (https://getbootstrap.com/)
   * Copyright 2011-2024 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
